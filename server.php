3fc9a539e54ce4d87cfcb0a1449f80ac80d6de16
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2020-04-09 12:23:17 -0500

    add: Authentification using AccessTokens

--- server.php
+++ server.php
@@ -1,25 +1,29 @@
 <?php
 
-if(
-    !array_key_exists('HTTP_X_HASH',$_SERVER) || 
-    !array_key_exists('HTTP_X_TIMESTAMP',$_SERVER) ||
-    !array_key_exists('HTTP_X_UID',$_SERVER))
-
-{
+if( !array_key_exists('HTTP_X_TOKEN',$_SERVER) ){
     die;
 }
 
-list( $hash, $uid, $timestamp ) = [
-    $_SERVER['HTTP_X_HASH'],
-    $_SERVER['HTTP_X_UID'],
-    $_SERVER['HTTP_X_TIMESTAMP'],
-];
+$url ='http://localhost:8001';
 
-$secret = 'Hora';
+$ch = curl_init( $url );
+curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    [
+        "X-Token: {$_SERVER['HTTP_X_TOKEN']}"
+    ]
+    );
 
-$newHash = sha1($uid.$timestamp.$secret);
+curl_setopt(
+    $ch,
+    CURLOPT_RETURNTRANSFER,
+    true
+);
 
-if( $newHash !== $hash){
+$ret = curl_exec( $ch );
+
+if($ret !== 'true'){
     die;
 }
 
