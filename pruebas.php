979d4b28f05e63e58bf363fdec452e2c231573a2
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2020-04-08 23:43:03 -0500

    add: created a file test

---
+++ pruebas.php
@@ -0,0 +1,24 @@
+<?php
+
+$books = [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    2 => [
+        'titulo' => 'La iliada',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+];
+
+
+//echo array_keys( $books );
+
+echo "[count($books) -1 ]";
